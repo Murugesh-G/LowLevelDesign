72bd0c5ae77386b6c4288cd164385ae499679cde
Murugesh Govindhappa <murugesh2311@gmail.com>
2025-02-22 16:04:36 +0530

    init commit

---
+++ src/main/java/com/mglld/App.java
@@ -0,0 +1,13 @@
+package com.mglld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
